6a9d301111f4755480e3f921d7e6e92a64d121bb
Erin Becker <erin.becker23@d155.org>
2022-08-22 08:43:27 -0500

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,27 @@
+fun main(){
+    //numbers
+    var x: Double = 10.0
+    var y: Double = 2.0
+
+    //operators
+    var diff: Double
+    var sum: Double
+    var quotient: Double
+    var multiply: Double
+
+    //solutions
+    diff = x-y
+    println(diff)
+
+    sum = x+y
+    println(sum)
+
+    quotient = x/y
+    println(quotient)
+
+    multiply = x*y
+    println(multiply)
+
+    println (x<=y)
+    println (x>=y)
+}
